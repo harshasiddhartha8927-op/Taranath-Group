79aeedf614beb771d211578ad177ce28becf837c
P.Harsha Siddhartha <harshasiddhartha8927@gmail.com>
2026-06-19 07:39:55 +0000

    just one

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   server: {
     port: 3000,
     strictPort: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        services: resolve(__dirname, 'services.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        verticals: resolve(__dirname, 'verticals.html')
+      }
+    }
   }
 });
